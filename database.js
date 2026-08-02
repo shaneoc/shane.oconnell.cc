3b1d8fdf8ab5505db8d655068569c589d050e17d
Shane O'Connell <shane@oconnell.cc>
2026-08-02 15:27:18 -0400

    Add journal entry

--- database.js
+++ database.js
@@ -606,6 +606,7 @@ const archive_database = [
 ["dir", "journal"],
 ["file", "2026-07-22-the-tenstorrent-groq-dream", "2026-07-22-the-tenstorrent-groq-dream.md"],
 ["file", "2026-08-01-the-dream-about-visiting-newfoundland", "2026-08-01-the-dream-about-visiting-newfoundland.md"],
+["file", "2026-08-02-movies-watched", "2026-08-02-movies-watched.md"],
 "up",
 ["dir", "pages"],
 ["file", "2026-06-12-printing-money", "2026-06-12-printing-money.md"],
